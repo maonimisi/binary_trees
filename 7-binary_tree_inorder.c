772a4a414678f03abc53964774fc37caf6485565
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-29 10:28:43 +0100

    Binary tree inorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,22 @@
+/*
 #include "binary_trees.h"
-/**
+**
  * binary_tree_inorder - Uses in-order traversal to traverse binary tree
  * @tree: Pointer to the tree to traverse
  * @func: A pointer to a function to call for each node
  * Description: If tree or func are NULL, do nothing
+ * Return: void
+ */
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - uses in-order traversal to traverse binary tree
+ * @tree: pointer to the tree to traverse
+ * @func: a pointer to a function to call for each node
+ *
+ * Description: if tree or func are NULL, do nothing
+ *
  * Return: void
  */
 
@@ -15,3 +28,26 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+} */
