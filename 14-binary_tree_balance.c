b65950526605ad8cb2004bc0d6fed282e70957fa
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-31 08:01:31 +0100

    function that measure binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height >= right_height)
